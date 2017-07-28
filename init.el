d354179a73ab0ba63cc179e26ae9a4219b8c39e0
SunflowerColor <yxd408012460@gmail.com>
2017-07-28 15:40:45 +0000

    最初带Python的设置

---
+++ init.el
@@ -0,0 +1,22 @@
+(require 'package)  
+(setq package-archives  
+      '(("gnu" . "http://elpa.gnu.org/packages/")  
+        ("marmalade" . "http://marmalade-repo.org/packages/")  
+        ("melpa" . "http://melpa.milkbox.net/packages/")))  
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (python-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+ (autoload 'python-mode "python-mode" "Python Mode." t)
+ (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+ (add-to-list 'interpreter-mode-alist '("python" . python-mode))
